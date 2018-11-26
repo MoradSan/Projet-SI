a8de202734afa130aed70bb731fcab601c253bc9
DoykFilt <thibaud.beaufils@gmail.com>
2018-11-26 15:26:54 +0100

    inkn

--- algo_flots_optiques.py
+++ algo_flots_optiques.py
@@ -10,7 +10,6 @@ COULEUR_INDESIRABLE = HSV(178, 174, 114)
 
 # Classe qui implemente l'algorithme Flot Optique
 class flot_optiques(algo.algorithme):
-    
     # Retourner le nom d'algorithme
     def get_nomAlgo(self):
         return "Flot optiques"
@@ -19,20 +18,20 @@ class flot_optiques(algo.algorithme):
     def traiterVideo(self, video, start_frame):
         ma_liste = list()
         cap = cv2.VideoCapture(video)
-        
+
         # Parametres pour ShiTomasi Corner Detection
         # maxcorner : nombre maximal de points
         # qualityLevel : influence la qualité du coin détecté
         # minDistance : distance minimum entre les détections
         # blockSiza : influence la qualité du calcul
         feature_params = dict(maxCorners=100, qualityLevel=0.01, minDistance=7, blockSize=7)
-        
+
         # Parametres pour Lucas Kanade optical flow
         # winSize : taille de la fenêtre de recherche du point proche
         # maxLevel : nombre de niveau de piramide (voir détail de l'algorithme
         lk_params = dict(winSize=(15, 15), maxLevel=2,
                          criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
-        
+
         # Création de couleurs aleatoire
         color = np.random.randint(0, 255, (100, 3))
 
@@ -43,7 +42,7 @@ class flot_optiques(algo.algorithme):
         # récupère les deux premières frames
         ret, old_frame = cap.read()
         ret, frame = cap.read()
-        
+
         # Récupère la zone d'intérer et réduit les frames à celle-ci
         if zi.ZoneInteret.verifier_presence_fichier_ini():
             with open("./zi/param.ini", "r") as file:
@@ -73,74 +72,10 @@ class flot_optiques(algo.algorithme):
                 # converti la frame en niveau de gris
                 frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-                #Calculer flot optique
-                #p0 et p1 deux tableaux de même taille
-                p1, st, err = cv2.calcOpticalFlowPyrLK(old_gray, frame_gray, p0, None, **lk_params)
-                #print(frame[int(p1[0]), int(p1[1])])
-
-                #Selectionner les bons points
-                good_new = p1[st == 1]
-                good_old = p0[st == 1]
-
-                """
-                norme_total = 0
-                nombre_points = 0
-                #Faire le dessin qui indique le changement de points caracteristiques
-                for i, (new, old) in enumerate(zip(good_new, good_old)):
-                    a, b = new.ravel()
-                    c, d = old.ravel()
-                    norme_total += math.sqrt((a - c) ** 2 + (b - d) ** 2)
-                    nombre_points += 1
-                    cv2.line(mask, (a, b), (c, d), color[i].tolist(), 2)
-                    cv2.circle(frame, (a, b), 5, color[i].tolist(), -1)
-                ma_liste.append(norme_total / nombre_points / 0.05)
-                """
-
-                #Affichage de la norme du centre de gravité
-                norme_total = 0
-                X = 0
-                Y = 0
-                nombre_points = 0
-                # Faire le dessin qui indique le changement de points caracteristiques
-                for i, (new, old) in enumerate(zip(good_new, good_old)):
-                    a, b = new.ravel()
-                    c, d = old.ravel()
-
-                    X += a
-                    Y += b
-                    nombre_points += 1
-
-                    cv2.line(mask, (a, b), (c, d), color[i].tolist(), 2)
-                    cv2.circle(frame, (a, b), 5, color[i].tolist(), -1)
-
-                if nombre_points > 0:
-                    norme_total += math.sqrt((X/nombre_points) ** 2 + (Y/nombre_points) ** 2)
-                    ma_liste.append(norme_total)
-
-                """#Affichage de la moyenne des x 
-                x = 0
-                n = 0
-                for i, (new, old) in enumerate(zip(good_new, good_old)):
-                    a, b = new.ravel()
-                    c, d = old.ravel()
-                    x += a
-                    n+=1
-                    cv2.line(mask, (a, b), (c, d), color[i].tolist(), 2)
-                    cv2.circle(frame, (a, b), 5, color[i].tolist(), -1)
-                ma_liste.append(x/n)
-                """
-
-                img = cv2.add(frame, mask)
-
-                cv2.imshow("frame", img)
-
-                k = cv2.waitKey(30) & 0xff
-                if k == 27:
-                    break
-
-                #Mise a jours le frame actuel
-                old_gray = frame_gray.copy()
-                p0 = good_new.reshape(-1, 1, 2)
+                # Calcule le flot optique à partir des deux frame et des points détectés sur la première
+                # p1, un nouvel ensemble de point
+                # st, un tableau de même taille que P0 et P1, pour chaque point 1 si une correspondance a été trouvé
+                # pour p0 dans p1, 0 sinon
                 p1, st, err = cv2.calcOpticalFlowPyrLK(old_gray, frame_gray, p0, None, **lk_params)
 
                 # Cas où l'ensemble de point n'est pas vide
