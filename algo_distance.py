d118d51285c0d27553b1f4a1e164e2ed0c2b442f
Konaté Mariam <32955466+mariamko25@users.noreply.github.com>
2018-12-18 23:11:49 +0100

    Update algo_distance.py

--- algo_distance.py
+++ algo_distance.py
@@ -18,20 +18,37 @@ class algo_distance(algo.algorithme):
     le nombre de changement est enregistré comme un indicateur de détection de stress
     @version 2.0
     """
-
+    incr=1
+    cameracopie=cv2.VideoCapture(0)
     def get_nomAlgo(self):
         return "distance"
 
-    def traiterVideo(self, video, start_frame):
+    def traiterVideo(self, video, start_frame,pd):
 
+        
+        global incr
         ma_liste = list()
         if type(video) is str:
+            global cameracopie
             if not os.path.exists(video):
                 raise Exception("Le fichier video '" + video + "' Introuvable!")
             cap = cv2.VideoCapture(video)
+            cameracopie= cv2.VideoCapture(video)
+            ret2, fraaame = cameracopie.read()
+            global incr
+            incr = 0
+            incr2=0
+            while ret2:
+                ret2, fraaame = cameracopie.read()
+                incr = incr + 1
+                incr2=incr2+1
+                if pd.value() < 30 and incr2 > 30:
+                    pd.setValue(pd.value() + 1)
+                    incr2=0
         else:
             cap = video
-
+        pd.setValue(30)
+        incrval = (70 / incr)
         # Detection de l'opérateur
         # On ne commence le traitement sur la première image dépourvu d'opérateur
         # On Passe les images
@@ -70,7 +87,10 @@ class algo_distance(algo.algorithme):
             # On applique les paramètres à chaque frame
             if ret and zi.ZoneInteret.verifier_presence_fichier_ini():
                 frame = frame[param[1]:param[1] + param[3], param[0]:param[0] + param[2]]
-
+            if pd.value() < 99 and int(incrval)>=1:
+                incrval=70/incr
+                pd.setValue(pd.value()+1)
+            incrval += 70/incr
         cap.release()
         cv2.destroyAllWindows()
         return ma_liste
